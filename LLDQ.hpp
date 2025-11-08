abdacd4dce79323dfa664fdd67e0c8631ed5e28e
asun9594-sys <asun9594@gmail.com>
2025-11-08 17:58:24 -0500

    implemented lldq

--- LLDQ.hpp
+++ LLDQ.hpp
@@ -19,22 +19,74 @@ private:
 
 public:
     // Constructor
-    LLDQ();
+    LLDQ() = default;
+
+    // ================================================================
+	//  ________________________________
+	// |                                |
+	// |          The Big Five          |
+	// |________________________________|
+
+    // Copy Constructor
+    LLDQ(const LLDQ& other) {
+        list = other.list;
+    }
+    
+    // Copy Assignment Operator
+    LLDQ& operator=(const LLDQ& other) {
+        if (this == &other) return *this;
+        list = other.list;
+        return *this;
+    }
+
+    // Move Constructor
+    LLDQ(LLDQ&& other) noexcept {
+        list = std::move(other.list);
+    }
+
+    // Move Assignment Operator
+    LLDQ& operator=(LLDQ&& other) noexcept {
+        if (this == &other) return *this;
+        list = std::move(other.list);
+        return *this;
+    }
+
+    // Destructor
+    ~LLDQ() = default;
+
+    // ================================================================
+	//  ________________________________
+	// |                                |
+	// |     Insertion and Removal      |
+	// |________________________________|
 
     // Core Insertion Operations
-    void pushFront(const T& item) override;
-    void pushBack(const T& item) override;
+    void pushFront(const T& item) override { list.addHead(item); }
+    void pushBack(const T& item) override { list.addTail(item); }
 
     // Core Removal Operations
-    T popFront() override;
-    T popBack() override;
+    T popFront() override { list.removeHead(); }
+    T popBack() override { list.removeTail(); }
+
+    // ================================================================
+	//  ________________________________
+	// |                                |
+	// |           Accessors            |
+	// |________________________________|
 
     // Element Accessors
-    const T& front() const override;
-    const T& back() const override;
+    const T& front() const override {
+        if(list.getHead() == nullptr) throw std::runtime_error("List is empty.");
+        return list.getHead()->data; 
+    }
+
+    const T& back() const override {
+        if(list.getTail() == nullptr) throw std::runtime_error("List is empty.");
+        return list.getTail()->data; 
+    }
 
     // Getter
-    std::size_t getSize() const noexcept override;
+    std::size_t getSize() const noexcept override { return list.getCount(); }
 };
 
 
